3da506da5ee2647218fb1913474ccf9d228c4b66
zhSHUVO <zhshuvo99@gmail.com>
2022-03-01 02:51:07 +0600

    display search result

--- app/script.js
+++ app/script.js
@@ -1,3 +1,4 @@
+// getting phone data
 const searchPhone = () => {
     const searchResult = document.getElementById("search-bar");
     const searchText = searchResult.value;
@@ -8,6 +9,28 @@ const searchPhone = () => {
         .then((data) => displaySearchResult(data.data));
 };
 
+// display search result
 const displaySearchResult = (phones) => {
-    console.log(phones);
+    const searchResult = document.getElementById("phone-card");
+    phones.forEach((phone) => {
+        console.log(phone);
+        const div = document.createElement("div");
+        div.classList.add("col", "d-flex", "justify-content-center");
+        div.innerHTML = `
+            <div class="card text-center w-75">
+                <img
+                    src="${phone.image}"
+                    class="card-img-top w-75 mx-auto p-3"
+                    alt=""
+                    
+                />
+                <div class="card-body">
+                    <h5 class="card-title">${phone.phone_name}</h5>
+                    <p class="card-text">${phone.brand}</p>
+                    <a href="#" class="btn detail-button">See Details</a>
+                </div>
+            </div>
+        `;
+        searchResult.appendChild(div);
+    });
 };
